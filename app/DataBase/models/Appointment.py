dc85a5938da61d5ee04a074a6745f44dcf8e24a3
AlexeyGrushev <yablokov2004@mail.ru>
2025-07-08 21:30:29 +0300

    Fix #2: Fix lint in files

--- app/DataBase/models/Appointment.py
+++ app/DataBase/models/Appointment.py
@@ -10,25 +10,19 @@ class Appointment(Base):
     __tablename__ = "Appointment"
     __table_args__ = (
         UniqueConstraint(
-            "doctor_id", "start_time",
-            name="uq_doctor_start_time"),
+            "doctor_id", "start_time", name="uq_doctor_start_time"
+        ),
     )
 
     id: Mapped[int] = mapped_column(
         BigInteger, primary_key=True, nullable=False, autoincrement=True
     )
-    doctor_id: Mapped[int] = mapped_column(
-        BigInteger, nullable=False
-    )
-    client_id: Mapped[int] = mapped_column(
-        BigInteger, nullable=False
-    )
+    doctor_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
+    client_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
     start_time: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), nullable=False
     )
     end_time: Mapped[datetime | None] = mapped_column(
         DateTime(timezone=True), nullable=True
     )
-    note: Mapped[str | None] = mapped_column(
-        VARCHAR(255), nullable=True
-    )
+    note: Mapped[str | None] = mapped_column(VARCHAR(255), nullable=True)
