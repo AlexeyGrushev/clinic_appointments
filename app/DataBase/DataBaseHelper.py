8af966669c0196b0ac66e85428046ea4cc005808
AlexeyGrushev <yablokov2004@mail.ru>
2025-07-08 17:20:25 +0300

    Removed unhelpful method

--- app/DataBase/DataBaseHelper.py
+++ app/DataBase/DataBaseHelper.py
@@ -2,7 +2,6 @@ from sqlalchemy import (
     delete,
     insert,
     select,
-    text,
 )
 
 from app.DataBase.Base import async_session
@@ -13,7 +12,7 @@ class DataBaseHelper:
     Base class for database manipulations
     """
 
-    model = None
+    model: None | type = None
 
     @classmethod
     async def find_all(cls, **kwargs):
@@ -43,16 +42,6 @@ class DataBaseHelper:
             result = await session.execute(query)
             await session.commit()
             return result.scalar_one()
-
-    @classmethod
-    async def get_seq(cls, *args, **kwargs) -> int:
-        if cls.model is None:
-            raise ValueError("Model is not set for DataBaseHelper subclass.")
-        async with async_session() as session:
-            seq = cls.model.__table__.name + "_id_seq"
-            query = text(f"select last_value from {seq}")
-            result = await session.execute(query)
-            return result.fetchone()[0]
 
     @classmethod
     async def delete_values(cls, *args, **kwargs):
